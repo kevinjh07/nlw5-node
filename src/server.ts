09a88dfde6be6f53d21adfc02e2eeae4fcf32855
Kevin Jhonson <kevinjh775@gmail.com>
2021-04-21 01:03:10 -0300

    Aula 02
    - Adiçao das dependencias para utilizar banco de dados sqlite3 e mapear entidades.
    - Alteraçao do tsconfig.json para permitir decorators, que serao utilizados nas entidades a serem persistidas no banco.
    - Criaçao da migration para a entidade Settings.
    - Adiçao da repository e controller para gerenciamento da entidade Settings.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
+import './database';
+import { routes } from './routes';
+
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'Olá NLW 05!',
-  });
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => console.log('Server is running on port 3333'));
